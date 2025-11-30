cceff475cc2242152725f07c44b61515eba97387
shaunakkarnik <shaunak.karnik.14@gmail.com>
2025-11-29 20:59:28 -0500

    created dataloader

--- vision_transformer.py
+++ vision_transformer.py
@@ -1,6 +1,14 @@
 import torch
 import torchvision
 
-train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True)
-test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True)
+# hyperparameters
+batch_size = 32
+num_workers = 2
+device = torch.device('mps')
 
+# load dataset and create dataloader
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=torchvision.transforms.ToTensor(), download=True)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, transform=torchvision.transforms.ToTensor(), download=True)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, num_workers=num_workers)
