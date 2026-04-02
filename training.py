a217666564daf3d62a7493c5c67e285bc3278099
shaunakkarnik <shaunak.karnik.14@gmail.com>
2026-04-02 17:36:10 -0400

    updated training script to reflect recent changes in notebook

--- training.py
+++ training.py
@@ -1,79 +1,159 @@
 import torch
-import torchvision
 from torch import nn
+from torch.utils.data import Subset
+from torch.utils.data import default_collate
+import torchvision
+from torchvision import transforms
+from torchvision.transforms import v2
+from PIL import Image
+import matplotlib.pyplot as plt
 from vision_transformer import VisionTransformer
 
-device = torch.device('mps')
+device = torch.device('cpu')
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    print("Using CUDA")
+else:
+    device = torch.device('mps')
+    print("Using MPS")
+
+train_transforms = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+val_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+cutmix = v2.CutMix(alpha=0.2, num_classes=10)
+mixup = v2.MixUp(alpha=0.2, num_classes=10)
+cutmix_or_mixup = transforms.v2.RandomChoice([cutmix, mixup])
+
+# instantiating twice to get different transforms on train vs. val
+full_train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=train_transforms, download=True)
+full_val_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=val_transforms, download=True)
+
+train_size = int(len(full_train_dataset) * 0.9)
+
+indices = torch.randperm(len(full_train_dataset))
+
+train_indices = indices[:train_size]
+val_indices = indices[train_size:]
+
+train_dataset = Subset(full_train_dataset, train_indices)
+val_dataset = Subset(full_val_dataset, val_indices)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, transform=val_transforms, download=True)
+
+batch_size = 512
+num_workers = 4
+
+def collate_fn(batch):
+    return cutmix_or_mixup(*default_collate(batch))
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True, collate_fn=collate_fn)
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=True)
 
 # training hyperparameters
-batch_size = 32
-num_workers = 2 # ?
-train_iters = 2
-eval_interval = 1
-eval_iters = 2
-
-# load full CIFAR-10 dataset
-full_train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=torchvision.transforms.ToTensor(), download=True)
-
-# split train into train and val
-train_size = int(len(full_train_dataset) * 0.9)
-val_size = len(full_train_dataset) - train_size
-
-train_dataset, val_dataset = torch.utils.data.random_split(full_train_dataset, [train_size, val_size])
-test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True)
-
-# create dataloaders
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, num_workers=2)
+train_iters = 325
+checkpoint_interval = 10
+lr = 4e-3
+weight_decay = 0.05
+label_smoothing = 0.0
+eps = 1e-8
 
 model = VisionTransformer().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
 
-# evaluation func to get avg val loss over eval_iters number of batches
+criterion = nn.CrossEntropyLoss(label_smoothing=label_smoothing)
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay, eps=eps)
+scheduler = torch.optim.lr_scheduler.OneCycleLR(
+    optimizer=optimizer, max_lr=lr, steps_per_epoch=len(train_loader), epochs=train_iters
+)
+scaler = torch.amp.GradScaler("cuda")
+
+def compute_accuracy(logits, labels):
+    preds = torch.argmax(logits, dim=1)
+    num_correct = (preds == labels).sum().item()
+    return num_correct
+
 @torch.no_grad()
-def eval(iteration):
+def evaluation(loader):
 
     model.eval()
 
-    val_losses = torch.zeros(eval_iters)
-    for i, (batch, labels) in enumerate(val_loader):
-        if i == eval_iters:
-            break
+    loss_sum = 0.0
+    correct_preds = 0
+    total = 0
+
+    for batch, labels in loader:
 
         x = batch.to(device)
         targets = labels.to(device)
-        out = model(x)
-        loss = criterion(out, targets)
-        val_losses[i] = loss.item()
 
-    avg_val_loss = val_losses.mean(dim=0)
+        with torch.amp.autocast("cuda"):
+            out = model(x)
+            loss = criterion(out, targets)
+
+        loss_sum += loss.item()
+
+        correct_preds += compute_accuracy(out, targets)
+        total += targets.size(0)
+
 
     model.train()
 
-    print("Epoch " + str(iteration) + " - Val loss: " + str(avg_val_loss.item()))
+    avg_loss = loss_sum / len(loader)
+    avg_acc = correct_preds / total
+
+    return avg_loss, avg_acc
+
+save_weights_path = "vit_best_8.pth"
+best_val_acc = 0.0
 
 # main training loop
 if __name__ == "__main__":
     for i in range(train_iters):
         train_losses_sum = 0.0
 
-        for batch_num, (batch, labels) in enumerate(train_loader):
-            
+        model.train()
+
+        for _, (batch, labels) in enumerate(train_loader):
+
             x = batch.to(device)
             targets = labels.to(device)
 
-            out = model(x)
-
-            loss = criterion(out, targets)
-            train_losses_sum += loss.item()
+            with torch.amp.autocast("cuda"):
+                out = model(x)
+                loss = criterion(out, targets)
 
             optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            scaler.scale(loss).backward()
+            scaler.unscale_(optimizer)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            scaler.step(optimizer)
+            scaler.update()
+            scheduler.step()
 
-        print("Epoch " + str(i) + " - Train loss: " + str(train_losses_sum / len(train_loader)))
+            train_losses_sum += loss.item()
 
-        if i % eval_interval == 0 or i == train_iters - 1:
-            eval(i)
+        avg_train_loss = train_losses_sum / len(train_loader)
+        avg_val_loss, avg_val_acc = evaluation(val_loader)
+
+        print(
+            f"Epoch: {i + 1}"
+            + f" Train Loss: {avg_train_loss:.4f}"
+            + f" Val Loss: {avg_val_loss:.4f}"
+            + f" Validation Accuracy: {avg_val_acc:.4f}"
+        )
+
+        if avg_val_acc > best_val_acc:
+            best_val_acc = avg_val_acc
+            torch.save(model.state_dict(), save_weights_path)
+
+        if (i + 1) % checkpoint_interval == 0 or i == train_iters - 1:
+            print(f"Best val acc: {best_val_acc:.4f}")
